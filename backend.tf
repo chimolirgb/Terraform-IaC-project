fc80ca116bcfa3f706b474f37eee511153984c06
chimolirgb <chimolilucy@gmail.com>
2025-03-08 16:20:42 +0300

    modified changes

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket"  # Change this to your S3 bucket
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "af-south-1"
     encrypt        = true
     use_lockfile = true
   }
--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "us-east-1"
+  default     = "af-south-1"
 }
 
 variable "vpc_id" {
