6f89617b990d5fa5205b4ba2a51f979735048644
chimolirgb <chimolilucy@gmail.com>
2025-03-08 13:10:10 +0300

    A VPC with public and private subnets

---
+++ modules/vpc/main.tf
@@ -0,0 +1,30 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = var.az
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = var.az
+
+  tags = {
+    Name = "Private Subnet"
+  }
+}
